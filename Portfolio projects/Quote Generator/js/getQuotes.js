f16ebb427e085f686801a361b05ae3fc5e8e845c
TheRealPaulyglot <PaulESanchezC@outlook.com>
2022-05-14 02:15:08 -0400

    naming... my god this was something
    i did that => getquotes(){
        getquotesAsync()
    ..}
    hahahahaha

--- Portfolio projects/Quote Generator/js/getQuotes.js
+++ Portfolio projects/Quote Generator/js/getQuotes.js
@@ -3,7 +3,7 @@ function getQuotes() {
   let quoteText = document.getElementById("quote");
   let getNewQuote = document.getElementById("new-quote");
 
-  async function getQuoutesAsync() {
+  async function fetchQuoutesAsync() {
     let apiUrl = "https://type.fit/api/quotes";
     let quotesArray = [];
     let randomIndex = 0;
@@ -30,7 +30,7 @@ function getQuotes() {
 
   async function getSingleQuote() {
     loading();
-    let quote = await getQuoutesAsync();
+    let quote = await fetchQuoutesAsync();
 
     if (quote.text.length > 100) {
       quoteText.classList.remove("quotes");
@@ -47,8 +47,6 @@ function getQuotes() {
   // On load
   getSingleQuote();
 
-  // Event listeners
-  // On Get New Quote
   getNewQuote.addEventListener("click", getSingleQuote);
 
   function loading() {
